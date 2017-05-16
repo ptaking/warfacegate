7af5b313f0e206b22a1cc6c684be54271c325ffb
Phạm Tuấn Anh <ratnhanhgon@gmail.com>
2017-05-16 17:00:22 +0700

    GoUser scaffolding

---
+++ app/Http/Controllers/GoUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GoUser;
+use Illuminate\Http\Request;
+
+class GoUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GoUser  $goUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GoUser $goUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\GoUser  $goUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GoUser $goUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GoUser  $goUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GoUser $goUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GoUser  $goUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GoUser $goUser)
+    {
+        //
+    }
+}
